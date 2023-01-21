c1ad4c6f66205f5fc2a7d9174af314ff44904a3a
hoon7566 <hoon7566@gmail.com>
2023-01-21 23:12:36 +0900

    실행 파일 생성

--- drink.py
+++ drink.py
@@ -9,16 +9,45 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 from time import sleep
+import sys, os
 import openpyxl
 
 import requests
 
-url = "http://dailydrink.co.kr/search"
+drink_url = "http://dailydrink.co.kr/search"
+
+
+def create_folder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
+
+def get_exe_file_path():
+    # print('bundle dir is', os.path.abspath(__file__))
+    # print('sys.argv[0] is', sys.argv[0])
+    # print('sys.executable is', sys.executable)
+    # print('os.getcwd is', os.getcwd())
+
+    if getattr(sys, 'frozen', False):
+        # we are running in a bundle
+        # bundle_dir = sys._MEIPASS
+        exe_dir = sys.argv[0]
+    else:
+        # we are running in a normal Python environment
+        exe_dir = os.path.dirname(os.path.abspath(__file__))
+    return exe_dir
+
 
 def crawling_dailydrink():
     dr = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    print('open chrome')
+
+    drink_path = get_exe_file_path()
     dr.implicitly_wait(10)  # second
-    dr.get(url)  # 드라이버를 통해 url의 웹 페이지를 오픈
+    dr.get(drink_url)  # 드라이버를 통해 url의 웹 페이지를 오픈
     a = dr.find_element(By.CLASS_NAME, 'list_more_btn').find_element(By.TAG_NAME, 'span')
     try:
         for i in range(100):
@@ -33,6 +62,8 @@ def crawling_dailydrink():
     drink_data = []
     drink_index = []
     drink_header = ['날짜', '제품명', '발매', '종류', '도수', '캐스크번호', '판매 상점/온라인', '결제', '가격', ]
+
+    create_folder(drink_path)
     for i in range(len(drink_list)):
         date = drink_list[i].select_one('li.date span').text
         name = drink_list[i].select_one('li.name span').text
@@ -45,8 +76,9 @@ def crawling_dailydrink():
         price = drink_list[i].select_one('li.price span').text
         drink_index.append(date)
         drink_data.append([date, name, made_year, type, percentage, cask_number, shop, p_type, price])
+    print(get_exe_file_path())
 
-    pd.DataFrame(drink_data, columns=drink_header).to_csv('./test.csv', index=False)
+    pd.DataFrame(drink_data, columns=drink_header).to_csv(drink_path + '/test.csv', index=False)
 
 
 if __name__ == '__main__':
